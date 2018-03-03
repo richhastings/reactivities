345ab0657ebac0e293fd6b0aa9455441174c7636
Rich Hastings <rich.hastings@clock.co.uk>
2018-03-03 13:14:06 +0000

    Initial commit

---
+++ src/components/primitive/navigation/container/Navigation.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import NavigationComponent from '../component/Navigation'
+
+class Navigation extends React.Component {
+  render() {
+    return (
+      <NavigationComponent />
+    )
+  }
+}
